c55862e2f68830e4e567fd0740a3ccdef94d59d4
Nilesh Solanki <nilesh.solanki@delcaper.com>
2023-03-11 16:09:15 +0530

    add mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mysql = require('mysql')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const db = mysql.createConnection({
@@ -13,6 +14,26 @@ db.connect((err) => {
       throw err;
   }
   console.log('MySql Connected...');
+});
+mongoose.connect('mongodb://unovziwckjgwp57we3qo:fxmwrpxaaC9WN3pWD1s7@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bxiipt5opbkbu6c?replicaSet=rs0')
+        .then(() => console.log('Mongodb Concted...'))
+        .catch(err => console.log('Database Not concted...', err));
+
+const userSchema = new mongoose.Schema({
+          name: {
+              type: String,
+              required: true
+          }
+      });
+const User = mongoose.model('User', userSchema);
+app.get('/user', async(req,res)=>{
+  const user = await User.find();
+  if (user) {
+    res.status(200).send(user)
+  }
+else {
+    res.status(400).send('data not found')
+}
 });
 
 app.get('/post',(req,res)=>{
